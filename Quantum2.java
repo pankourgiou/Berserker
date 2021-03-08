ad9a8f6b04ea9d677907e69c766bc998b7a5445d
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-03-08 04:07:19 +0200

    Create Quantum2.java

---
+++ Quantum2.java
@@ -0,0 +1,25 @@
+public class Quantum2 {  
+public static void main(String[] args) {
+
+int time = 10;
+if (time < 10) {
+  System.out.println("E = mc**2");
+} else if (time < 9) {
+  System.out.println("E = mcmod2.");
+} else  if (time < 8){
+  System.out.println("e(sqrt82(sqrt(e)) = evolve.");
+} else  if (time < 7){
+  System.out.println("128(sqrt(e)980 = I love you");
+} else  if (time < 6){
+  System.out.println("Eht = chmod+x");
+} else  if (time < 5){
+  System.out.println("Eros = Death");
+} else  if (time < 4){
+  System.out.println("Salt"); 
+} else  if (time < 3){
+  System.out.println("Pepper");  
+} else  if (time < 2){
+  System.out.println("We need more Alpharithmetical");  
+}
+}
+}
